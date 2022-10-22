337e3035b00fa79b49d7ef7b2987504ff5e4e478
Kojobu <91381250+Kojobu@users.noreply.github.com>
2022-10-22 12:13:13 +0200

    Update gustav-rp-bot.py

--- gustav-rp-bot.py
+++ gustav-rp-bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 import pickle
 
 TOKEN = 'DISCORD_TOKEN'
-GUILD = 'DISCORD_GUILD' #NOT REQUIRED
+GUILD = 'DISCORD_GUILD'
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,6 +18,7 @@ class characters:
                 self.chars = dict(pickle.load(f))
         else:
             self.chars = dict()
+
     def add(self, name, attributes):
         
         self.chars[name] = np.asarray(attributes, dtype = int)
@@ -37,6 +38,21 @@ class characters:
     def throw(self):
         return self.chars
 
+    def pos(self, name, ability):
+        name = name.lower()
+        scores = self.give(name)
+        if ability.capitalize() in nabil_scor:
+            pos = np.where(ability.capitalize() == np.array(nabil_scor))[0][0]
+        elif ability in snabil_scor:
+            pos = np.where(ability == np.array(snabil_scor))[0][0]
+        else:
+            pos = None
+        return pos, scores
+
+    def modify(self, name, pos, value):
+        self.chars[name.lower()][pos] += value
+        self.save()
+
 bot = commands.Bot(intents=intents , command_prefix='!' , description='Gustav - The roleplay assistant')
 
 chars = characters()
@@ -49,9 +65,9 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
 @bot.command()
-async def add_char(ctx, arg1=-1, arg2=0): #arg1 - assign points freely, arg2 - give value and Gustav assigns the ability points random
-    arg1, arg2 = int(arg1), int(arg2)
-    ability_scores = 20 if arg1 == -1 else arg1
+async def add_char(ctx, points=20 ,random=0): #arg1 - assign points freely, arg2 - give value and Gustav assigns the ability points random
+    random, points = int(random), int(points)
+    ability_scores = points
     await ctx.send("Please give your character a name:")
 
     def check(m):
@@ -60,7 +76,7 @@ async def add_char(ctx, arg1=-1, arg2=0): #arg1 - assign points freely, arg2 - g
     msg = await bot.wait_for("message", check=check)
     name = msg.content.lower()
 
-    if arg2 == 1:
+    if random == 1:
         await ctx.send('Creating random character ...')
         ab_sc = np.full(6,0)
         while ability_scores > 0:
@@ -111,37 +127,46 @@ async def stat(ctx, arg):
     pass
 
 @bot.command()
-async def roll(ctx, name, score, thresh):
-    name = name.lower()
-    scores = chars.give(name)
-    if score.capitalize() in nabil_scor:
-        pos = np.where(score.capitalize() == np.array(nabil_scor))[0][0]
-    elif score in snabil_scor:
-        pos = np.where(score == np.array(snabil_scor))[0][0]
-    else:
-        await ctx.send('Ability score not found.')
+async def mod_char(ctx, name, ability, value):
+    value = int(value)
+    pos, _ = chars.pos(name, ability)
+    chars.modify(name, pos, value)
+    word = "added to" if value>=0 else "subtracted from"
+    await ctx.send(f"{abs(value)} points were successfully " + word + f""" {name.capitalize()}'s {nabil_scor[pos]}.""")
 
+@bot.command()
+async def roll(ctx, name, ability):
+
+    pos, scores = chars.pos(name, ability)
+    
     randint = np.random.randint(1,21)
     if scores[pos] >= randint:
-        await ctx.send(f'{nabil_scor[pos]}-Check successful! ({randint} / {scores[pos]})')
+        if randint == 20:
+            await ctx.send(f'### {nabil_scor[pos]} check was a critical success! ({randint} / {scores[pos]}) ###')
+        else:
+            await ctx.send(f'{nabil_scor[pos]} check successful! ({randint} / {scores[pos]})')
     else:
-        await ctx.send(f'{nabil_scor[pos]}-Check failed! ({randint} / {scores[pos]})')
+        if randint == 1:
+            await ctx.send(f'### {nabil_scor[pos]} check was a critical failure! ({randint} / {scores[pos]}) ###')
+        else:
+            await ctx.send(f'{nabil_scor[pos]} check failed! ({randint} / {scores[pos]})')
 
 @bot.command()
 async def info(ctx):
     await ctx.send("""Gustav rp-bot v1.0\n
-    @Tom Schlenker
-    https://github.com/Kojobu/discord-dnd_rp-bot/
+    Code by Tom Schlenker\n
+    https://github.com/Kojobu/discord-dnd_rp-bot
     """)
 
 @bot.command()
 async def helpme(ctx):
-    await ctx.send("""A "!" denotes a command, "{}" denotes optional arguments (when ending with ?, a bool is required), capitalized arguments are manditory
-    ```!add_char {#POINTS} {RANDOM?}``` - Adds a character with 20 or {#POINTS} ability scores. If random is desired set {RANDOM?}=1. {#POINTS}=-1 for standard number of points (20)
+    await ctx.send("""A command must start with "!", "{}" denotes optional arguments
+    ```!add_char {#POINTS} {RANDOM?}``` - Adds a character with {#POINTS} ability points (base value is 20 if not given). If {RANDOM?} flag is set 1, ability score will be assigned randomly. 
     ```!del_char NAME``` - Removes the character from the database
     ```!show_char NAME``` - Returns the ability scores of the given character
     ```!list_char``` - Returns a list of all saved characters with the corresponing ability scores
-    ```!roll NAME SCORE THRESHOLD``` - Check character's ability score against given threshold (d20 roll, (rolled / character's ability))
+    ```!roll NAME SCORE``` - Check character's ability score (d20 roll, (rolled / character's ability))
+    ```!mod_char NAME SCORE VALUE``` - Adds / subtracts given amount of ability points to character's score
     ```!test WORD``` - Returns WORD (for debug purposes only)
     ```!stat``` - Returns statistics about rolls and probabilities (not yet implemented)
     ```!info``` - Gives info about Gustav
