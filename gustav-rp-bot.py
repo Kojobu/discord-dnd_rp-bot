1f42b5da05618a00484670ad9664b281a6b5c755
Allomere <91381250+Kojobu@users.noreply.github.com>
2023-08-21 13:48:22 +0200

    Update gustav-rp-bot.py
    Latest version of Gustav (deprecated, see Gostav)

--- gustav-rp-bot.py
+++ gustav-rp-bot.py
@@ -4,8 +4,15 @@ import numpy as np
 import discord
 from discord.ext import commands
 import pickle
+from bs4 import BeautifulSoup
+import requests
+import time
+from matplotlib import pyplot as plt
+import matplotlib
+import datetime as dt
+import lxml
 
-TOKEN = 'DISCORD_TOKEN'
+TOKEN = ''
 GUILD = 'DISCORD_GUILD'
 
 intents = discord.Intents.default()
@@ -33,7 +40,7 @@ class characters:
             pickle.dump(self.chars,f)
     
     def give(self, name):
-        return self.chars[name.lower()]
+        return seslf.chars[name.lower()]
 
     def throw(self):
         return self.chars
@@ -164,7 +171,7 @@ async def helpme(ctx):
     ```!add_char {#POINTS} {RANDOM?}``` - Adds a character with {#POINTS} ability points (base value is 20 if not given). If {RANDOM?} flag is set 1, ability score will be assigned randomly. 
     ```!del_char NAME``` - Removes the character from the database
     ```!show_char NAME``` - Returns the ability scores of the given character
-    ```!list_chars``` - Returns a list of all saved characters with the corresponing ability scores
+    ```!list_char``` - Returns a list of all saved characters with the corresponing ability scores
     ```!roll NAME SCORE``` - Check character's ability score (d20 roll, (rolled / character's ability))
     ```!mod_char NAME SCORE VALUE``` - Adds / subtracts given amount of ability points to character's score
     ```!test WORD``` - Returns WORD (for debug purposes only)
@@ -172,4 +179,65 @@ async def helpme(ctx):
     ```!info``` - Gives info about Gustav
     """)
     
+@bot.slash_command(name="first_slash") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_slash(ctx): 
+    await ctx.respond("You executed the slash command!")
+
+@bot.slash_command(name="ozon") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def ozon(ctx): 
+    '''Returns current ozon concentration in Heidelberg'''
+    url = "https://weather.com/forecast/air-quality/l/464ff4f67462799509e60caea6f814062d58eb77833cfa81b374cb8be899dc07"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    ozon_val = soup.find_all('main')[0].find_all('section')[1].find_all('span')[7].contents
+    await ctx.respond(f'Current ozon concentration is: {ozon_val[0]}')
+
+@bot.slash_command(name="mensa") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def mensa(ctx): 
+    """Print the mensa food for Zentralmensa HD"""
+    url = 'https://www.studentenwerk.uni-heidelberg.de/de/speiseplan_neu'
+    data = requests.get(url).text
+    soup = BeautifulSoup(data, 'lxml')
+    food_bs4 = soup.find_all('table')[9:11]
+    food = """Today's food is:\n"""
+    food += str(food_bs4[0].find_all('td')[15])[4:-5]
+    food += """\n\nTomorrow's food is:\n"""
+    food += str(food_bs4[1].find_all('td')[15])[4:-5]
+    await ctx.respond(food)
+
+@bot.slash_command(name='shutdown')
+@commands.is_owner()
+async def stop(ctx):
+    await ctx.respond('Goodbye!')
+    exit()
+
+@bot.slash_command(name='plot')
+async def plot(ctx, data):
+    '''Enter a list of datapoint, seperated by ',', and I'll plot it for you'''
+    y = np.asarray(data.split(","),dtype=float)
+    plt.plot(y)
+    plt.savefig('temp.jpg')
+    await ctx.send(f"Your plot is ready!", file=discord.File("temp.jpg"))
+
+
+@bot.slash_command(name='b-field')
+async def bfield(ctx):
+    '''Plot the local B-field of my room'''
+    with open('/home/berry-potato/Documents/mag/sensorsave.dat') as f:
+        raw = np.loadtxt(f)
+    mes = raw[0::2]
+    t = raw[1::2]
+    timestamps = [dt.datetime.fromtimestamp(ts) for ts in t]
+    mes /= mes.mean()
+    fig, ax = plt.subplots()
+    ax.plot(timestamps, mes)
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%d-%m-%Y %H:%M:%S'))
+    plt.xticks(rotation=25)
+    plt.xlabel('Time')
+    plt.ylabel('Relative, Absolute Field Strength')
+    plt.title('Relative Magnetic Field vs Time')
+    fig.tight_layout()
+    plt.savefig('./mag.png')
+    await ctx.send(file=discord.File('mag.png'))
+
 bot.run(TOKEN)
